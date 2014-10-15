a8c149ca0b3469dd0c2fd8bb3fb76a9aab0e66b9
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-10-15 09:01:29 +0900

    new file:   .gitignore 	modified:   test2.php

--- test2.php
+++ test2.php
@@ -6,3 +6,12 @@ $a = "あaいiうuえお";
 print mb_detect_encoding($a)."\n";
 var_dump(preg_split("//u", $a, -1, PREG_SPLIT_NO_EMPTY));
 
+
+$dir = ".";
+
+include_once('locatefile.class.php');
+$a = new LocateFile();
+
+$list = $a->getFileList($dir);
+var_dump($list);
+
