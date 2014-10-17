ac03d98775b51a5c1c091421c52c97308c663db3
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-10-17 23:19:19 +0900

    modified:   test2.php

--- test2.php
+++ test2.php
@@ -8,11 +8,14 @@ var_dump(preg_split("//u", $a, -1, PREG_SPLIT_NO_EMPTY));
 */
 
 
+$r = preg_replace('/(.*)(\/\/|#).*/', '\1', 'aiueo // # あいうえお');
+print $r."\n";
+
 $dir = ".";
 
 include_once('locatefile.class.php');
 $a = new LocateFile();
 
 $list = $a->getFileList($dir);
-var_dump($list);
+//var_dump($list);
 
