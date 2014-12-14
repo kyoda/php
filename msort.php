20a8e08952468fb5f02fc4b90215e39b6b8ddb76
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-12-14 19:56:27 +0900

    modified:   msort.php

--- msort.php
+++ msort.php
@@ -5,9 +5,15 @@ function cmp($a, $b)
   return strcmp($a['num'], $b['num']);
 }
 
+function rcmp($a, $b)
+{
+  return strcmp($a['id'], $b['id']);
+}
+
 $arr = array(array('id' => 0, 'num' => 5), array('id' => 1, 'num' => 8), array('id' => 3, 'num' => 4));
 
 usort($arr, 'cmp');
-
+var_dump($arr);
+usort($arr, 'rcmp');
 var_dump($arr);
 
