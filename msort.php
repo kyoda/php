73db70a5ed99f656b4e38cc5cfffa4360d640002
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-12-16 22:46:44 +0900

    modified:   msort.php

--- msort.php
+++ msort.php
@@ -12,8 +12,10 @@ function rcmp($a, $b)
 
 $arr = array(array('id' => 0, 'num' => 5), array('id' => 1, 'num' => 8), array('id' => 3, 'num' => 4));
 
+$st = microtime(true);
 usort($arr, 'cmp');
 var_dump($arr);
 usort($arr, 'rcmp');
 var_dump($arr);
+printf("%f sec\n", microtime(true) - $st);
 
