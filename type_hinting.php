5732e601bff0b996d474217455c88e22b5e87ed1
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-12-28 15:43:38 +0900

    modified:   type_hinting.php

--- type_hinting.php
+++ type_hinting.php
@@ -4,14 +4,22 @@
 class TypeHinting 
 {
 
+  private $VERSION = 0.1;
+
   public function dumpArr(array $str)
   {
     var_dump($str); 
+  }
+
+  public function dumpObj(TypeHinting $obj)
+  {
+    var_dump($obj);
   }
 
 }
 
 $a = new TypeHinting();
 $a->dumpArr(array("abcdefg"));
+$a->dumpObj($a);
 
 
